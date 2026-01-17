b6566dad72b6e172e29b592ead3eb04aaf505d98
Beatrice Womack <bunbea77@gmail.com>
2026-01-17 12:26:54 -0600

    Add room data model with size confirmation

---
+++ src/main/java/com/vacuum/model/Door.java
@@ -0,0 +1,19 @@
+package com.vacuum.model;
+
+/**
+ * Placeholder Door class - will be implemented in future PR This stub allows Room to compile.
+ */
+public class Door {
+    // Stub to allow Room class to compile
+    // Full implementation coming in next PR
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
